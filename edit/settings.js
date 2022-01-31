f7cb1cf7c2b0d282e54e2e94bc73431d15bb958c
tioMomonga <96915218+tioMomonga@users.noreply.github.com>
2022-01-30 23:11:01 -0300

    Add files via upload

---
+++ edit/settings.js
@@ -0,0 +1,5 @@
+donoY = '554991579631'; //use sem espaços, e sem o 9 adicional
+
+exports.prefix = '-'; //prefixo padrão do bot. Você também pode ativar o multiprefix, basta digitar .multuprefix (e ativar com o botão)
+
+exports.dono = donoY + '@s.whatsapp.net'; //não altere
